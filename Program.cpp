53740fe45240a44e0e4c2ec89585f92c3391e06c
FRMS <frms@tamu.edu>
2023-03-06 13:13:57 -0600

    First file add with some functions

---
+++ Program.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <exception>
+#include <math.h>
+
+using namespace std;
+
+string goalString = "Congradulations you win!!!";
+
+//final string
+void Goal(bool a){
+
+    if(a == true){
+        cout<< goalString << endl;
+    }
+    
+}
+
+
+//input
+string a(string inp){
+
+}
+string b(string inp){
+
+}
+
+//attepmpts
+//rename after done
+bool attempts(string red){
+
+}
+
+//hash function
+bool validate(string s){
+
+}
+
+
+
+int main(){
+    string input;
+    cin >> input;
+
+}
